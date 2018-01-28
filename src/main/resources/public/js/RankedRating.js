07fa80c413d8aa56895e93f467b0329e9dd88e4c
Jae Yang <jxy4769@rit.edu>
2018-01-28 01:53:25 -0500

    fixed rank bug: Apikey is passed down

--- src/main/resources/public/js/RankedRating.js
+++ src/main/resources/public/js/RankedRating.js
@@ -1,10 +1,9 @@
-function RankedRating(id){
+function RankedRating(id, apiKEY){
   
   var header = document.querySelector('header');
   var section = document.querySelector('section');
   var proxy = 'https://cors-anywhere.herokuapp.com/';
-  var apiKey = 'RGAPI-099247db-98fb-4345-a4aa-cd091eaa75bb';
-  var apiRequestURL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+id+'?api_key='+apiKey;
+  var apiRequestURL = 'https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+id+'?api_key='+apiKEY;
   var requestURL = proxy + apiRequestURL;
   var request = new XMLHttpRequest();
   var id;
@@ -23,12 +22,7 @@ function RankedRating(id){
   }
   function getRank(jsonObj) {
 	var myH1 = document.createElement('h1');
-	if(jsonObj['rank']==undefined){
-		myH1.innerHTML += "Current Rank: Unranked";
-	}
-	else{
-	myH1.textContent = "Current Rank: " + jsonObj['rank'];
-	}
+	myH1.textContent = "Current Rank: " + jsonObj[0]['tier'] + " "+ jsonObj[0]['rank'];
 	header.appendChild(myH1);
   }
 }
