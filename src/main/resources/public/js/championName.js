43f73a8ec028f454c6046ff22b2ffd9d06f0e1b9
Jae Yang <jxy4769@rit.edu>
2018-01-28 03:42:32 -0500

    Local JSON Champ Data

--- src/main/resources/public/js/championName.js
+++ src/main/resources/public/js/championName.js
@@ -1,27 +1,17 @@
 function championName(id,apiKEY){
-  
+  //id is champID
   var header = document.querySelector('header');
   var section = document.querySelector('section');
   var proxy = 'https://cors-anywhere.herokuapp.com/';
   var apiRequestURL = 'https://na1.api.riotgames.com/lol/static-data/v3/champions/'+id+'?locale=en_US&champData=image&api_key='+ apiKEY;
   var imgLoadingURL = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'
   var requestURL = proxy + apiRequestURL;
-  var request = new XMLHttpRequest();
-  function callOtherDomain(){
-	if(request){
-		request.open('GET', requestURL);
-		request.responseType = 'json';
-		request.send();
-	}
+
+
+  function getBestChamp() {
+	var obj = champList.data[id];
+	document.getElementById("mostPlayed").innerHTML += ("Most Played Champion Name: " + obj['name']);
+	document.getElementById("champPortrait").src = imgLoadingURL + obj['name'] + '_0.jpg';
   }
-  callOtherDomain();
-  request.onload = function() {
-    var userInfo = request.response;
-    getBestChamp(userInfo);
-	
-  }
-  function getBestChamp(jsonObj) {
-	document.getElementById("mostPlayed").innerHTML += ("Most Played Champion Name: " + jsonObj['name']);
-	document.getElementById("champPortrait").src = imgLoadingURL + jsonObj['name']+ '_0.jpg';
-  }
+  getBestChamp();
 }
